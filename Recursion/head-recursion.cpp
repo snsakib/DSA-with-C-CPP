7f44b010105ade711674c2d7f070a18e3d8ca6f2
snsakib <hello@snsakib.com>
2021-12-30 15:45:58 +0600

    head recursion

---
+++ Recursion/head-recursion.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// Head recursion
+void fun(int n)
+{
+    if (n > 0)
+    {
+        fun(n - 1);
+        printf("%d\n", n);
+    }
+};
+
+int main()
+{
+    int n = 3;
+    fun(n);
+    return 0;
+}
